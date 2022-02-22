15faa87155867a0c671eeed8f25d911d1186766a
Kelly Hum <kellyhum88@gmail.com>
2022-02-21 18:54:41 -0800

    mapped li data instead of writing out li's individually

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,25 @@
+import buttonlist from './buttonlist'
+
 const Nav = () => {
     return (  
         <nav>
+            <div>
+
+            </div>
+            
             <ul>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#exp">Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
-                </li>
+                {
+                    buttonlist.map((button) => (
+                        <li key={button.key}>
+                            {<a href={button.link}>{button.title}</a>}
+                        </li>
+                    ))
+                }
             </ul>
 
-            <div></div>
+            <button>
+                
+            </button>
         </nav>
     );
 }
