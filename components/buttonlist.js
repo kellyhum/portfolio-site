25d743317e0bb976dfa8306854d0b00ec793e859
Kelly Hum <kellyhum88@gmail.com>
2022-02-21 18:56:24 -0800

    created file to store button data

---
+++ components/buttonlist.js
@@ -0,0 +1,19 @@
+const buttonlist = [
+    {
+        "title": "About",
+        "link": "#about",
+        "key": 1
+    },
+    {
+        "title": "Projects",
+        "link": "#projects",
+        "key": 2
+    },
+    {
+        "title": "Experience",
+        "link": "#experience",
+        "key": 3
+    }
+]
+
+export default buttonlist;
