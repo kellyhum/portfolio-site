f5e678a659164143e62753b815783f1cadb39afe
Kelly Hum <kellyhum88@gmail.com>
2022-02-21 18:56:53 -0800

    created project card component

---
+++ components/ProjectCard.js
@@ -0,0 +1,15 @@
+const ProjectCard = () => {
+    return (  
+        <div>
+            <img></img>
+
+            <div>
+                <h6>A lil blurb</h6>
+                <h3>Title</h3>
+                <p>Main blurb</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default ProjectCard;
