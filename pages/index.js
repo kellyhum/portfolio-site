01b922ad20f0b95c25890b0f20eeea4a71e6c7d3
Kelly Hum <kellyhum88@gmail.com>
2022-02-25 12:14:26 -0800

    added landing section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Meta from '@components/Meta'
 import Nav from '@components/Nav'
+import Section from '@components/Section'
 
 export default function Home() {
   return (
-    <main className='h-screen w-screen bg-brown'>
+    <main className='h-screen w-screen text-brown'>
       <Meta /> {/*get metadata*/}
 
       <Nav /> {/*import navigation menu*/}
+
+      <Section id={'#'} title={'Hi, I\'m Kelly!'}> {/*landing section*/}
+        <p>
+          Welcome to my digital space!  This is where you’ll find everything from my full scale projects to half-baked ideas.
+          Feel free to poke around, or take your time and scroll!
+        </p>
+      </Section>
+
     </main>
   )
 }
