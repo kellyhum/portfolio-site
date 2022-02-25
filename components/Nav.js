8692b895768c0d0777f7e54922e3e8699c8deb5b
Kelly Hum <kellyhum88@gmail.com>
2022-02-25 12:13:21 -0800

    added more styling, changed buttonlist import

--- components/Nav.js
+++ components/Nav.js
@@ -1,25 +1,25 @@
-import buttonlist from './buttonlist'
+import buttonlist from '../public/js/buttonlist'
 
 import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 const Nav = () => {
     return (  
-        <nav className='flex items-center justify-between px-8 py-8'>
+        <nav className='font-title flex items-center justify-between px-14 py-8'>
             <div>
-
+                <h3>KHum</h3>
             </div>
             
-            <ul className='flex list-none'>
+            <ul className='flex list-none ml-auto mr-10'>
                 {
                     buttonlist.map((button) => (
-                        <li className='mx-4 px-4 py-1 font-title text-beige hover:bg-caramel/50 hover:rounded-lg transition duration-75' key={button.key}>
+                        <li className='mx-4 px-4 py-1 rounded-lg text-l hover:underline-offset-1 duration-100' key={button.key}>
                             {<a href={button.link}>{button.title}</a>}
                         </li>
                     ))
                 }
             </ul>
 
-            <button className='p-2 rounded-lg'>
+            <button className='p-2 rounded-lg bg-caramel/30'>
                 <ModeNightIcon />
             </button>
         </nav>
