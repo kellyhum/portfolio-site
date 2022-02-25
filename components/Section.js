87c0dc7a158cc65dabf7f81ef1f0ee59bdbbf3de
Kelly Hum <kellyhum88@gmail.com>
2022-02-25 13:25:09 -0800

    changed font family and size

--- components/Section.js
+++ components/Section.js
@@ -1,7 +1,7 @@
 const Section = ({id, title, children}) => {
     return (  
         <div id={id} className="px-section-side py-section-top">
-            <h1 className="font-title font-black tracking-wider text-5xl">{title}</h1>
+            <h1 className="font-title font-black tracking-wider text-landing-title">{title}</h1>
             {children}
         </div>
     );
