ea86b1a9f1a126241e8b6a05c7b97736065ecb29
Kelly Hum <kellyhum88@gmail.com>
2022-02-24 18:53:49 -0800

    added colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['"Open Sans"', 'sans-serif'],
+      'title': ['Ubuntu', 'sans-serif']
+    },
+    colors: {
+      'brown': '#3C352B',
+      'caramel': '#A47F48',
+      'beige': '#EBE0DA'
+    },
     extend: {},
   },
   plugins: [],
