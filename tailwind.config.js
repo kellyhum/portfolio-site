5e1514d3f59357742de205438f272e8ab229777f
Kelly Hum <kellyhum88@gmail.com>
2022-02-26 16:54:56 -0800

    set darkmode to switch according to class

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     fontSize: {
       'landing-title': '4rem',
