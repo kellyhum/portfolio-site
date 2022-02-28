713092f080dc58aee7a5c1c6b57fb3934fac5463
Kelly Hum <kellyhum88@gmail.com>
2022-02-27 23:54:56 -0800

    adjusted height and letter spacing values

--- components/Section.js
+++ components/Section.js
@@ -1,7 +1,7 @@
 const Section = ({id, title, children}) => {
     return (  
-        <div id={id} className="px-section-side py-section-top">
-            <h1 className="font-title font-black tracking-wider text-landing-title">{title}</h1>
+        <div id={id} className={"px-section-side py-section-top h-full"}>
+            <h1 className="font-title font-black tracking-wide text-landing-title">{title}</h1>
             {children}
         </div>
     );
