67865829a0a6956e422638dcbcfa12097430a789
Kelly Hum <kellyhum88@gmail.com>
2022-02-27 23:51:54 -0800

    created project list file

---
+++ public/js/projectlist.js
@@ -0,0 +1,22 @@
+const projectlist = [
+    {
+        'title': 'Lorem Ipsum',
+        'description': 'blah blah blah',
+        'blurb': 'toodles',
+        'techstack': ['React', 'Tailwind CSS']
+    },
+    {
+        'title': 'Lorem Ipsum',
+        'description': 'blah blah blah',
+        'blurb': 'toodles',
+        'techstack': ['React', 'Tailwind CSS', 'Javascript', 'HTML']
+    },
+    {
+        'title': 'Lorem Ipsum',
+        'description': 'blah blah blah',
+        'blurb': 'toodles',
+        'techstack': ['React', 'Tailwind CSS', 'Python']
+    }
+]
+
+export default projectlist;
