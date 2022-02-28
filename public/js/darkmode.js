ef17f2f3260f24de093dabf218840a36af400be2
Kelly Hum <kellyhum88@gmail.com>
2022-02-27 23:55:48 -0800

    attempts at a localstorage dark mode

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -1,6 +1,9 @@
 const toggle = () => {
     const current = document.documentElement.classList.toggle('dark');
     localStorage.setItem('theme', current);
+
+    const savedTheme = localStorage.getItem('theme')
+    console.log(savedTheme)
 }
 
 export default toggle;
