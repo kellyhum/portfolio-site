a537bfff587f4a13e65ccf9de1ebffe508ae0b8d
Kelly Hum <kellyhum88@gmail.com>
2022-04-03 21:39:29 -0700

    revamped navigation to reflect new design

--- components/Nav.js
+++ components/Nav.js
@@ -1,29 +1,39 @@
 import buttonlist from '../public/js/buttonlist'
 import toggle from '../public/js/darkmode'
 
-import ModeNightIcon from '@mui/icons-material/ModeNight';
+import ModeNightIcon from '@mui/icons-material/ModeNightOutlined';
 
 const Nav = () => {
     return (  
-        <nav className='text-nav flex items-center justify-between px-14 py-8'>
-            <div>
-                <h1>KHum</h1>
-            </div>
-            
-            <ul className='flex list-none ml-auto mr-10'>
-                {
-                    buttonlist.map((button) => (
-                        <li className='mx-4 px-4 py-1 rounded-lg text-l hover:underline duration-100' key={button.key}>
-                            {<a href={button.link}>{button.title}</a>}
-                        </li>
-                    ))
-                }
-            </ul>
+        <div className='w-full h-20 px-44 flex items-center justify-between font-title'>
+            <h1 className='font-bold'>KHum</h1>
 
-            <button onClick={toggle} className='p-2 rounded-lg text-caramel bg-caramel/30 dark:bg-beige/30 dark:text-beige'>
-                <ModeNightIcon />
-            </button>
-        </nav>
+            <nav className='flex'>            
+                <ul className='flex justify-between align-middle font-medium'>
+                    {
+                        buttonlist.map((button) => (
+                            <li key={button.key} className='p-2.5 text-opacity-85'>
+                                {<a href={button.link} 
+                                className='inline-block relative 
+                                before:content-none 
+                                before:absolute 
+                                before:left-0
+                                before:bottom-0
+                                before:w-0 
+                                before:h-1
+                                before:bg-black
+                                hover:before:w-full'>{button.title}</a>}
+                            </li>
+                        ))
+                    }
+                </ul>
+
+                <button onClick={toggle} className='p-2.5 rounded-md hover:bg-golden transition-colors'>
+                    <ModeNightIcon /> 
+                </button>
+            </nav>
+        </div>
+        
     );
 }
  
