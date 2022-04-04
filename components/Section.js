88564c5be4caffcf02779c53a5f3823a6590a005
Kelly Hum <kellyhum88@gmail.com>
2022-04-03 21:38:12 -0700

    modified styles and added line break

--- components/Section.js
+++ components/Section.js
@@ -1,7 +1,8 @@
 const Section = ({id, title, children}) => {
     return (  
-        <div id={id} className={"px-section-side py-section-top h-full"}>
-            <h1 className="font-title font-black tracking-wide text-landing-title">{title}</h1>
+        <div id={id} className="px-44 h-full w-full">
+            <h1 className="font-title font-bold text-5xl">{title}</h1>
+            <hr></hr>
             {children}
         </div>
     );
