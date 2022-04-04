a8494bb816f7628b8f221626cd9d1a36a896a39e
Kelly Hum <kellyhum88@gmail.com>
2022-04-03 21:37:40 -0700

    removed blurb value

--- public/js/projectlist.js
+++ public/js/projectlist.js
@@ -2,19 +2,16 @@ const projectlist = [
     {
         'title': 'Lorem Ipsum',
         'description': 'blah blah blah',
-        'blurb': 'toodles',
         'techstack': ['React', 'Tailwind CSS']
     },
     {
         'title': 'Lorem Ipsum',
         'description': 'blah blah blah',
-        'blurb': 'toodles',
         'techstack': ['React', 'Tailwind CSS', 'Javascript', 'HTML']
     },
     {
         'title': 'Lorem Ipsum',
         'description': 'blah blah blah',
-        'blurb': 'toodles',
         'techstack': ['React', 'Tailwind CSS', 'Python']
     }
 ]
