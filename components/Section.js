5db5da0883a65b211335ccc7b9eea18667e94d5a
Kelly Hum <kellyhum88@gmail.com>
2022-04-04 21:33:51 -0700

    adjusted horizontal padding and line length

--- components/Section.js
+++ components/Section.js
@@ -1,8 +1,8 @@
 const Section = ({id, title, children}) => {
     return (  
-        <div id={id} className="px-44 h-full w-full">
+        <div id={id} className="px-80 h-full w-full">
             <h1 className="font-title font-bold text-5xl">{title}</h1>
-            <hr></hr>
+            <hr className="my-10 h-2 w-3/4 ml-auto"></hr>
             {children}
         </div>
     );
