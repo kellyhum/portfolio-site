f368c5e3a181f6b5f45f37dd4c5021e047a15574
Kelly Hum <kellyhum88@gmail.com>
2022-04-05 21:31:48 -0700

    switched div to section, modified height/width styles

--- components/Section.js
+++ components/Section.js
@@ -1,10 +1,10 @@
 const Section = ({id, title, children}) => {
     return (  
-        <div id={id} className="px-80 h-full w-full">
+        <section id={id} className="px-80 py-28 w-full">
             <h1 className="font-title font-bold text-5xl">{title}</h1>
             <hr className="my-10 h-2 w-3/4 ml-auto"></hr>
             {children}
-        </div>
+        </section>
     );
 }
  
