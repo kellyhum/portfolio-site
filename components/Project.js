cf6df4e7cf743c77ecf72021ec2ec0aed7c6c8ed
Kelly Hum <kellyhum88@gmail.com>
2022-04-05 21:30:51 -0700

    removed parameters for testing, added styles

--- components/Project.js
+++ components/Project.js
@@ -1,14 +1,10 @@
-const Project = ({title, description, techstack}) => {
+const Project = ({title, description}) => {
     return (  
-        <div>
+        <div className='flex-1 w-full p-10 border rounded-2xl'>
             <h3>{title}</h3>
             <p>{description}</p>
             <h4>
-                {
-                    techstack.map((item) => (
-                        {item}
-                    ))
-                }
+                
             </h4>
         </div>
     );
