981fc81721e4f41d16c5421d49a68cfff5064eca
Kelly Hum <kellyhum88@gmail.com>
2022-04-06 22:32:17 -0700

    made some minor font adjustments, added sample techstack for testing purposes

--- components/Project.js
+++ components/Project.js
@@ -1,11 +1,9 @@
 const Project = ({title, description}) => {
     return (  
         <div className='flex-1 w-full p-10 border rounded-2xl'>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <h4>
-                
-            </h4>
+            <h3 className="text-3xl font-title pb-5">{title}</h3>
+            <p className="pb-5">{description}</p>
+            <h4 className='font-title'>Next.js | Tailwind</h4>
         </div>
     );
 }
