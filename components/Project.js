1f4b6a18ecf56019fbe0b10e0bbcbe8f5634b805
Kelly Hum <kellyhum88@gmail.com>
2022-04-07 21:02:19 -0700

    added hover effect and test link

--- components/Project.js
+++ components/Project.js
@@ -1,9 +1,11 @@
 const Project = ({title, description}) => {
     return (  
-        <div className='flex-1 w-full p-10 border rounded-2xl'>
-            <h3 className="text-3xl font-title pb-5">{title}</h3>
-            <p className="pb-5">{description}</p>
-            <h4 className='font-title'>Next.js | Tailwind</h4>
+        <div className='w-full p-10 border rounded-2xl hover:shadow-projectshadow transition-all'>
+            <h3 className="text-3xl font-title">{title}</h3>
+            <hr className="my-5"></hr>
+            <p>{description}</p>
+            <h4 className='font-semibold my-3'>Next.js | Tailwind</h4>
+            <a className="underline underline-offset-1 hover:text-coral transition-colors">Github</a>
         </div>
     );
 }
