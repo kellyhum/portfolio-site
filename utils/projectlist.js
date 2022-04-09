3ebc5639ad90d96e269d8c867631f527527f0c09
Kelly Hum <kellyhum88@gmail.com>
2022-04-09 15:51:41 -0700

    added project descriptions

--- utils/projectlist.js
+++ utils/projectlist.js
@@ -1,23 +1,28 @@
 const projectlist = [
     {
-        'title': 'Lorem Ipsum',
-        'desc': 'blah blah blah',
-        'techstack': ['React', 'Tailwind CSS'],
-        'github': 'https://github.com',
+        'title': 'this site',
+        'desc': 'A place for showcasing project.  I started by creating a mockup on Figma and designing a colour palette.\
+                Using the mockup, I began developing the site using Next.js alongside Tailwind CSS for styling.',
+        'techstack': ['Next.js', 'Tailwind CSS', 'Figma'],
+        'link': 'https://github.com/kellyhum/portfolio-site',
         'key': 'a'
     },
     {
-        'title': 'Chicken Wings',
-        'desc': 'just another test',
-        'techstack': ['React', 'Tailwind CSS', 'Javascript', 'HTML'],
-        'github': '',
+        'title': 'redacted',
+        'desc': 'A lorem ipsum generator featuring paragraphs with random cat, dog, and foot text.\
+                One of my first projects, I wanted to build  something using arrays (there was a lot of splicing happening)\
+                that also had a visual component.',
+        'techstack': ['Javascript', 'CSS', 'HTML', 'Figma'],
+        'link': 'https://github.com/kellyhum/redacted',
         'key': 'b'
     },
     {
-        'title': 'Lorem Ipsum',
-        'desc': 'blah blah blah',
-        'techstack': ['React', 'Tailwind CSS', 'Python'],
-        'github': 'sdf',
+        'title': 'course planning site',
+        'desc': 'Worked with a team of 5 members to create a course planning site used by 1200+ students.  I was also in charge of\
+                teaching members how to use Wordpress and Divi and outlining sections of the site that needed to be worked on.\
+                Other responsibilities included relaying information with administrators and completing any other modifications needed.',
+        'techstack': ['Wordpress', 'Divi Builder'],
+        'link': 'https://mycentennial.sd43.bc.ca/courseplanning/',
         'key': 'c'
     }
 ]
