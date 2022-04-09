d6f53d4e8928fd9e14740857ecff00ee87fe867a
Kelly Hum <kellyhum88@gmail.com>
2022-04-09 15:52:11 -0700

    added different font sizes, colors, and box shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,29 @@ module.exports = {
       'sans': ['"Open Sans"', 'sans-serif'],
       'title': ['Ubuntu', 'sans-serif']
     },
+    fontSize: {
+      'footer': '0.875rem',
+      'default': '1rem',
+      'project': '1.875rem',
+      'heading': '3rem',
+      'title': '3.5rem'
+    },
     colors: {
       'beige': '#F6F5F0',
       'black': '#2D2D2B',
       'golden': '#FBD786',
-      'cherry': '#9b2226'
+      'cherry': '#9b2226',
+      'coral': '#f28482',
+      'teal': '#83c5be',
+      'dark-gold': '#d4af47'
     },
     extend: {
       padding: {
         'side': '25vw'
       },
       boxShadow: {
-        'projectshadow': '4px 4px 0 4px #FBD786'
+        'projectshadow': '6px 4px 0 4px #FBD786',
+        'projectshadowdark': '6px 4px 0 4px rgba(242, 132, 130, 0.9)'
       }
     }
   },
