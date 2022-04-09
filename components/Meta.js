89e51c1db6d589faeeecbf3c43296cfd4a527d5d
Kelly Hum <kellyhum88@gmail.com>
2022-04-09 15:53:03 -0700

    add favicon, update title and description

--- components/Meta.js
+++ components/Meta.js
@@ -6,13 +6,17 @@ const Meta = ({title, description, keywords}) => {
             <title>{title}</title>
             <meta name='description' content={description}></meta>
             <meta name='keywords' content={keywords}></meta>
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png"></link>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png"></link>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png"></link>
         </Head>
     )
 }
 
 Meta.defaultProps = {
-    title: 'Kelly Hum | Personal Site',
-    description: 'A student and developer. Curious about cybersecurity, accessible tech, and privacy within tech.'
+    title: 'Kelly Hum',
+    description: 'A student and developer. Curious about cybersecurity, accessible tech, and writing.'
 }
 
 export default Meta;
