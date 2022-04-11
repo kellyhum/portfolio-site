f4cc021c4553c123a097adb412d4783c9c2a01d1
Kelly Hum <kellyhum88@gmail.com>
2022-04-10 18:01:56 -0700

    switch to h2 so that headings are in descending order

--- components/Section.js
+++ components/Section.js
@@ -1,7 +1,7 @@
 const Section = ({ id, title, children }) => {
   return (
     <section id={id} className="px-side py-20 w-full">
-      <h1 className="font-title font-bold text-heading mb-10">{title}</h1>
+      <h2 className="font-title font-bold text-heading mb-10">{title}</h2>
       {children}
     </section>
   );
