0f24b4706b021660a8f95b870c731a1487a5ebf6
Kelly Hum <kellyhum88@gmail.com>
2022-04-18 10:05:28 -0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import "../styles/globals.css";
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-GYQX99GQ1R"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-GYQX99GQ1R');
+        `}
+      </Script>
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp;
